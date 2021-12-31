eef78f93276f5213a0b49d3ad9f092ad9b6f092a
akio0911 <akio0911@gmail.com>
2021-12-31 23:09:47 +0900

    feedback

--- Hiro42Kadai15/AddFruitViewController.swift
+++ Hiro42Kadai15/AddFruitViewController.swift
@@ -7,27 +7,40 @@
 
 import UIKit
 class AddFruitViewController: UIViewController {
+    enum Mode {
+        case create
+        case update(name: String)
+    }
+
     @IBOutlet private weak var enterFruitTextField: UITextField!
 
     private var didTapCancel: () -> Void = {}
     private var didTapSave: (String) -> Void = { _ in }
-    /// 課題16追加
-    var defaultText: CheckItem?
+
+    private var mode: Mode!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        /// 課題16追加
-        enterFruitTextField.text = defaultText?.name ?? ""
+
+        switch mode! {
+        case .create:
+            break
+        case .update(name: let name):
+            enterFruitTextField.text = name
+        }
     }
 
     @IBAction private func executeSaveButton(_ sender: Any) {
         guard let text = enterFruitTextField.text, !text.isEmpty else { return }
         didTapSave(text)
     }
+
     @IBAction private func executeCancelButton(_ sender: Any) {
         didTapCancel()
     }
-    func setup(didTapSave: @escaping (String) -> Void, didTapCancel: @escaping () -> Void) {
+
+    func setup(mode: Mode, didTapSave: @escaping (String) -> Void, didTapCancel: @escaping () -> Void) {
+        self.mode = mode
         self.didTapSave = didTapSave
         self.didTapCancel = didTapCancel
     }
